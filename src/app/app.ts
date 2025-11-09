891af57b89e22df4bfd0a4a3bfdf26ba4ec44308
Salma-Laanani <salmalaanani29@gmail.com>
2025-11-09 16:54:52 +0100

    comm1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
+
 })
 export class App {
   protected readonly title = signal('ecom-web-app');
